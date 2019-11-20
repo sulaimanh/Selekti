f953fa388fd0f97c602a009ac896327a6177e00f
Jasper Chong <chong_j@outlook.com>
2019-11-20 11:31:24 -0500

    switched to using logistic regression for PM

--- trunk/personalModel/personal_model.py
+++ trunk/personalModel/personal_model.py
@@ -3,7 +3,7 @@ from keras.applications import VGG16
 from keras.applications import imagenet_utils
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import LogisticRegression
 import os
 import pickle
 import numpy as np
@@ -25,18 +25,7 @@ class PersonalModel:
             model_file.close()
             print("[INFO] Using existing model.")
         else:
-            self.model = SGDRegressor(loss='huber',		#TODO: Change to Ridge or SVR because we'll have less than 100K samples
-                                penalty='l2', 
-                                alpha=0.0001, 
-                                fit_intercept=False, 
-                                n_iter=5, 
-                                shuffle=True, 
-                                verbose=1, 
-                                epsilon=0.1, 
-                                random_state=42, 
-                                learning_rate='invscaling', 
-                                eta0=0.01, 
-                                power_t=0.5)
+            self.model = LogisticRegression(solver="newton-cg", multi_class="multinomial")
             print("[INFO] Using new model.")
 
     def saveModel(self):
